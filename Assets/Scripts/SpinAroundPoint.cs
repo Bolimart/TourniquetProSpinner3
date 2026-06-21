1407ff99e562c97bddb5ffd140a92aad5250afd1
Audran Meyrignac <audran.meyrignac@etu.u-bordeaux.fr>
2026-06-22 00:39:20 +0200

    speedUp la rotation,Condition de défaite,En vole du perso,spawn de la foule

--- Assets/Scripts/SpinAroundPoint.cs
+++ Assets/Scripts/SpinAroundPoint.cs
@@ -9,8 +9,17 @@ public class SpinAroundPoint : MonoBehaviour
 
     public Vector3 origin;
     public Vector3 axis = new Vector3(0, 1, 0);
-    public float angle;
+    //public float angle;
+
+   public float rotationSpeed = 10f;
     private Transform _transform;
+
+    //speed Up every 10 seconds
+    private float speedUpTiming = 10f;
+
+    private float lastSpeedUpTime = 0f;
+
+    [SerializeField] private GameEvent _gameEvent;
     
     // ———— Unity events ————
     
@@ -21,8 +30,22 @@ public class SpinAroundPoint : MonoBehaviour
 
     void Update()
     {
+        float deltaAngle = rotationSpeed * Time.deltaTime;
         // Maybe update the rotation not at all frame for better performance ? With an event ?
-        _transform.RotateAround(origin, axis, angle);
+        _transform.RotateAround(origin, axis, deltaAngle);
+        if(Time.time - lastSpeedUpTime > speedUpTiming)
+        {
+            speedUp();
+        }
+    }
+
+    void speedUp()
+    {
+        lastSpeedUpTime = Time.time;
+        Debug.Log("Speeding up rotation");
+        rotationSpeed *= 2f;
+        //here now move it with score later
+        _gameEvent.Score100();
     }
     
     // ———— Methods ————
